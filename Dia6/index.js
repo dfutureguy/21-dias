0c3a2b1e2d1877c0094fb5776d85c4e1a4c1394c
Rangel Maia <rangel.maia@gmail.com>
2024-03-16 00:29:19 -0500

    learning about for

---
+++ Dia6/index.js
@@ -0,0 +1,16 @@
+//let numero = Number(prompt("Escolha um número:"))
+
+//for (i=0; i<=numero; i++){
+//    console.log(i)
+//}
+
+let novo = 50;
+for (i = novo;i>=0;i=i-5){
+    console.log(i)
+}
+
+
+
+
+
+
